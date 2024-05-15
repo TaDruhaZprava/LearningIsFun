14d94dd4b017356be4b119173bac093e76874539
TaDruhaZprava <167241538+TaDruhaZprava@users.noreply.github.com>
2024-05-15 18:07:02 +0200

    Add files via upload
    slightly corrected HW2

--- Koudelkova_Anna_hw02.py
+++ Koudelkova_Anna_hw02.py
@@ -3,34 +3,34 @@ import math
 import json
 import csv
 
-netflix = pd.read_csv("netflix_titles.tsv", sep='\t')
-netflix = netflix.drop(["TCONST", 'TITLETYPE', 'ORIGINALTITLE', 'ISADULT',
+netflix = pd.read_csv('netflix_titles.tsv', sep='\t')
+netflix = netflix.drop(['TCONST', 'TITLETYPE', 'ORIGINALTITLE', 'ISADULT',
                         'ENDYEAR', 'RUNTIMEMINUTES', 'AVERAGERATING',
                         'NUMVOTES', 'TITLETYPE_NEW', 'SHOW_ID', 'TYPE', 'TITLE',
                         'COUNTRY', 'DATE_ADDED', 'RELEASE_YEAR', 'RATING', 'DURATION',
-                        'LISTED_IN', 'DESCRIPTION'], axis="columns")
+                        'LISTED_IN', 'DESCRIPTION'], axis='columns')
 
-netflix = netflix[["PRIMARYTITLE", "DIRECTOR", "CAST", "GENRES", "STARTYEAR"]]
+netflix = netflix[['PRIMARYTITLE', 'DIRECTOR', 'CAST', 'GENRES', 'STARTYEAR']]
 
 netflix.to_csv('netflix.csv', sep=',', header=False,
                index=False, mode='w', encoding='UTF-8')
 
-netflix = []
-with open('netflix.csv', encoding="utf-8") as file:
-    data = csv.DictReader(file, delimiter=",", fieldnames=[
-                          "title", "directors", "cast", "genres", "decade"])
+netflix_composed = []
+with open('netflix.csv', encoding='utf-8') as file:
+    data = csv.DictReader(file, delimiter=',', fieldnames=[
+                          'title', 'directors', 'cast', 'genres', 'decade'])
     for line in data:
-        netflix.append(line)
+        netflix_composed.append(line)
 
-for n in netflix:
-    n["decade"] = math.floor(int(n["decade"])/10) * 10
-    n["directors"] = n["directors"].split(",")
-    n["cast"] = n["cast"].split(",")
-    n["genres"] = n["genres"].split(",")
-    if n["directors"] == [""]:
-        n["directors"] = []
-    elif n["cast"] == [""]:
-        n["cast"] = []
+for n in netflix_composed:
+    n['decade'] = math.floor(int(n['decade'])/10) * 10
+    n['directors'] = n['directors'].split(', ')
+    n['cast'] = n['cast'].split(', ')
+    n['genres'] = n['genres'].split(', ')
+    if n['directors'] == ['']:
+        n['directors'] = []
+    elif n['cast'] == ['']:
+        n['cast'] = []
 
-with open("hw02_output.json", mode="w", encoding="utf-8") as output_file:
-    json.dump(netflix, output_file, ensure_ascii=False, indent=4)
+with open('hw02_output.json', mode='w', encoding='utf-8') as output_file:
+    json.dump(netflix_composed, output_file, ensure_ascii=False, indent=4)
